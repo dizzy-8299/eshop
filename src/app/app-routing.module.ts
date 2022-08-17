1ef9eb8cde75a2c0a6a9333923022a5edb4c668b
dizzy-8299 <85520189+dizzy-8299@users.noreply.github.com>
2022-08-17 23:41:14 +0530

    added shared module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  {
-    path: 'admin',
-    loadChildren: () =>
-      import('./admin/admin.module').then((x) => x.AdminModule),
-  },
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
